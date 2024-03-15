d279e91100c6be7eb50feb1cb22d052bd7dba5c4
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-15 03:56:25 +0200

    Opens socket
    Listen 2291 on the host OS:
    $ python -m http.server 2291

---
+++ lab2/task2/task2.cpp
@@ -0,0 +1,76 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() 
+{
+    LPCSTR ipv4 = "127.0.0.1";
+    LPCSTR port = "2291";
+
+    WSADATA wsaData;
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    struct addrinfo* result = NULL, * ptr = NULL, hints;
+
+    // Initialize Winsock
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) 
+    {
+        std::cout << "WSAStartup failed: " << iResult << "\n";
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(ipv4, port, &hints, &result);
+    if (iResult != 0) 
+    {
+        std::cout << "getaddrinfo failed: " << iResult << "\n";
+        WSACleanup();
+        return 1;
+    }
+
+    // Attempt to connect to the first address returned by the call to getaddrinfo
+    ptr = result;
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+    // Check for errors to ensure that the socket is a valid socket
+    if (ConnectSocket == INVALID_SOCKET) 
+    {
+        std::cout << "Error at socket: " << WSAGetLastError() << "\n";
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    // Connect to server.
+    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) 
+    {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+    }
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) 
+    {
+        std::cout << "Unable to connect to server!\n";
+        WSACleanup();
+        return 1;
+    }
+
+    // Connection successful
+    std::cout << "Successfully connected to localhost on port 8080\n";
+
+    // cleanup
+    closesocket(ConnectSocket);
+    WSACleanup();
+
+    return 0;
+}
