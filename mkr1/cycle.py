ab2d2b07d57d207a3eb48103aee7c9894bc9b4bc
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-24 04:32:22 +0200

    WIth functions

--- mkr1/cycle.py
+++ mkr1/cycle.py
@@ -1,28 +1,37 @@
 from pwn import *
 
-seq = cyclic(5000).decode()
-print(seq)
 
-print("\n---\n")
-overflow = cyclic_find("aadi")  # rax before cmp eax, 0x539
-print(overflow)
+def get_breaks(code):
+    lines = code.strip().split('\n')
+    for line in lines:
+        addr = line.split(":")[1].split(" ")[0]
+        print(f"br *0x{addr}")
 
-print("\n---\n")
-overflow = cyclic_find("aaqo")
-print(overflow)
 
-print("\n---\n")
-# IDA code:
-text = """
-.text:08049ECC                 lea     esp, [ebp-0Ch]
-.text:08049ECF                 pop     ecx
-.text:08049ED0                 pop     ebx
-.text:08049ED1                 pop     edi
-.text:08049ED2                 pop     ebp
-.text:08049ED3                 lea     esp, [ecx-4]
-.text:08049ED6                 retn
-"""
-lines = text.strip().split('\n')
-addresses = ["0x" + line.split(' ')[0].split(':')[1] for line in lines]
-for addr in addresses:
-    print("br *" + addr)
+def main():
+    # Create de Bruijn sequence
+    seq = cyclic(5000).decode()
+    print(seq)
+
+    print("\n------------")
+    print(cyclic_find("aadi"))  # rax before cmp eax, 0x539
+
+    print("\n------------")
+    print(cyclic_find("aaqo"))
+
+    print("\n------------")
+    # IDA code:
+    code = """
+    .text:08049ECC                 lea     esp, [ebp-0Ch]
+    .text:08049ECF                 pop     ecx
+    .text:08049ED0                 pop     ebx
+    .text:08049ED1                 pop     edi
+    .text:08049ED2                 pop     ebp
+    .text:08049ED3                 lea     esp, [ecx-4]
+    .text:08049ED6                 retn
+    """
+    get_breaks(code)
+
+
+if __name__ == "__main__":
+    main()
