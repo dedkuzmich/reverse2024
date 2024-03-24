9de5170d840550ebbeaf6f2b2ec4aa7a9054fb6a
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-24 03:26:51 +0200

    Update cycle.py

--- mkr1/cycle.py
+++ mkr1/cycle.py
@@ -1,25 +1,26 @@
 from pwn import *
 
-seq = cyclic(3000).decode()
+seq = cyclic(5000).decode()
 print(seq)
 
 print("\n---\n")
-
-overflow = cyclic_find(0x69646161)  # rax before cmp eax, 0x539
+overflow = cyclic_find("aadi")  # rax before cmp eax, 0x539
 print(overflow)
 
 print("\n---\n")
+overflow = cyclic_find("aaqo")
+print(overflow)
 
+print("\n---\n")
 # IDA code:
 text = """
-.text:0804A077 lea     esp, [ebp-10h]
-.text:0804A07A pop     ecx
-.text:0804A07B pop     ebx
-.text:0804A07C pop     esi
-.text:0804A07D pop     edi
-.text:0804A07E pop     ebp
-.text:0804A07F lea     esp, [ecx-4]
-.text:0804A082 retn
+.text:08049ECC                 lea     esp, [ebp-0Ch]
+.text:08049ECF                 pop     ecx
+.text:08049ED0                 pop     ebx
+.text:08049ED1                 pop     edi
+.text:08049ED2                 pop     ebp
+.text:08049ED3                 lea     esp, [ecx-4]
+.text:08049ED6                 retn
 """
 lines = text.strip().split('\n')
 addresses = ["0x" + line.split(' ')[0].split(':')[1] for line in lines]
